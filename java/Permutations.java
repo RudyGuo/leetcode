358dc1fb25a98c0b65a9fe3b8f89f811dd967a66
GuoDi <guorudi@foxmail.com>
2014-10-10 20:23:14 +0800

    add length of last word.java

--- java/Permutations.java
+++ java/Permutations.java
@@ -4,6 +4,7 @@ For example,
 [1,2,3] have the following permutations:
 [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
  */
+ 
 
 public class Solution {
 	public List<List<Integer>> permute(int[] num) {
