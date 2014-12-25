4e21c091a8e21afe60fa49c1b0cfdcd516fb617c
GuoDi <guorudi@foxmail.com>
2014-12-25 21:53:35 +0800

    modify Word Break.java

--- java/Word Break.java
+++ java/Word Break.java
@@ -27,3 +27,33 @@ public class Solution {
     	}
     }
 }
+
+//参考九章算法，提取出set里字符串的最长距离，可以进一步减小算法
+public class Solution {
+	boolean f[];
+	int maxLen = Integer.MIN_VALUE;
+
+    public boolean wordBreak(String s, Set<String> dict) {
+    	f = new boolean[s.length()];
+    	maxLength(dict);
+    	wordBreak(s,dict,0);
+    	return f[s.length()-1];
+    }
+    
+    private void wordBreak(String s, Set<String> dict,int index){
+    	if(index>=s.length()) return;
+    	for(int i=index;i<maxLen+index;i++){
+    		if(i<s.length()&&dict.contains(s.substring(index,i+1))&&f[i]==false){
+    			f[i] = true;
+    			wordBreak(s,dict,i+1);
+    		}
+    	}
+    }
+    
+    private void maxLength(Set<String> dict){
+    	Iterator<String> it = dict.iterator();
+    	while(it.hasNext()){
+    		maxLen = Math.max(maxLen, it.next().length());
+    	}
+    }
+}
